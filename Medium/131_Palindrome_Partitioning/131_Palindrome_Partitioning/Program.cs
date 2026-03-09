2dc0985ac9719d608a53d402b1c984290a4573c7
Bohdan-Voznyi <daster.bohdan708@gmail.com>
2026-03-09 18:23:57 +0200

    Task 131 solved

---
+++ Medium/131_Palindrome_Partitioning/131_Palindrome_Partitioning/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _131_Palindrome_Partitioning
+{
+    /*
+        Example 1:
+
+        Input: s = "aab"
+        Output: [["a","a","b"],["aa","b"]]
+        Example 2:
+
+        Input: s = "a"
+        Output: [["a"]]
+     */
+
+
+    public class Solution
+    {
+        public IList<IList<string>> Partition(string s)
+        {
+            IList<IList<string>> list = new List<IList<string>>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+
+            }
+
+            return list;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
