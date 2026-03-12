81ddcdbc80b140197fd3de843168129dfde134dc
Bohdan-Voznyi <daster.bohdan708@gmail.com>
2026-03-12 18:02:42 +0200

    Task 1 solved

---
+++ Easy/1_TwoSum/1_TwoSum/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _1_TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var seenNumbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length;i++)
+            {
+                int a = target - nums[i];
+
+
+                if(seenNumbers.TryGetValue(a, out int complementIndex))
+                {
+                    return new int[] { complementIndex, i };
+                }
+
+                seenNumbers.Add(nums[i], i);
+
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            var result = sol.TwoSum([2,4,5,6,8,9,6,5],10);
+        }
+    }
+}
